148202260ec89cfef501fb613a075bb81810a4fb
Ed Lee <jonexiu@gmail.com>
2020-06-29 15:36:09 +0800

    delete console.log

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -11,7 +11,6 @@ import "../../styles/global.scss"
 import { getLang, calSizes, resizeThrottler } from "../../helpers/utils"
 
 const Layout = ({ children }) => {
-  console.log()
   const [sizes, setSizes] = useState({ isMobile: false })
   const [lang, setLang] = useState("zh")
   const [mousePos, setMousePos] = useState({ x: -1000, y: -1000 })
