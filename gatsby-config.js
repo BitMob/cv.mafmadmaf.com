5a5729c3ca705addc5bc1c361877f65330ff8cc8
maf <fredmamono@gmail.com>
2020-07-01 22:03:21 +0800

    的！！！！

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const contentDir = `${__dirname}/src/content`
 
 module.exports = {
   siteMetadata: {
-    title: `mafの設計十週年紀念館 · 2011 - 2020`,
+    title: `maf 的設計十週年紀念館 · 2011 - 2020`,
     description: `About maf, a designer`,
     author: `maf`,
   },
